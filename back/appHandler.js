8acbd902472a347fae3f55a110fa876702b4d0d2
hans555 <onghansheng@gmail.com>
2022-07-22 11:39:35 +0800

    update handlers with sample formatting

--- back/appHandler.js
+++ back/appHandler.js
@@ -1,10 +1,88 @@
 const { pool, promisePool } = require("./database");
 const jsonic = require("jsonic");
 
+//Functionality 1
 async function handleLogin(req, res) {
   const body = jsonic(req.body);
   try {
-    const [rows, fields] = await promisePool.query("SELECT rate from exchange_rate");
+    const [rows, fields] = await promisePool.query(
+      "SELECT rate from exchange_rate"
+    );
+    console.log(rows);
+    res.status(200).json({});
+  } catch (err) {
+    console.log(err);
+    res.status(400).end();
+  }
+}
+
+//Functionality 2
+async function handleGetExchangeRate(req, res) {
+  const body = jsonic(req.body);
+  try {
+    const [rows, fields] = await promisePool.query(
+      "SELECT rate from exchange_rate"
+    );
+    console.log(rows);
+    res.status(200).json({});
+  } catch (err) {
+    console.log(err);
+    res.status(400).end();
+  }
+}
+
+//Functionality 3
+async function handleGetCurrencyWallet(req, res) {
+  const body = jsonic(req.body);
+  try {
+    const [rows, fields] = await promisePool.query(
+      "SELECT rate from exchange_rate"
+    );
+    console.log(rows);
+    res.status(200).json({});
+  } catch (err) {
+    console.log(err);
+    res.status(400).end();
+  }
+}
+
+//Functionality 4
+async function handlePostTransaction(req, res) {
+  const body = jsonic(req.body);
+  try {
+    const [rows, fields] = await promisePool.query(
+      "SELECT rate from exchange_rate"
+    );
+    console.log(rows);
+    res.status(200).json({});
+  } catch (err) {
+    console.log(err);
+    res.status(400).end();
+  }
+}
+
+//Functionality 5
+async function handleGetWallet(req, res) {
+  const body = jsonic(req.body);
+  try {
+    const [rows, fields] = await promisePool.query(
+      "SELECT rate from exchange_rate"
+    );
+    console.log(rows);
+    res.status(200).json({});
+  } catch (err) {
+    console.log(err);
+    res.status(400).end();
+  }
+}
+
+//Functionality 6
+async function handleDeleteWallet(req, res) {
+  const body = jsonic(req.body);
+  try {
+    const [rows, fields] = await promisePool.query(
+      "SELECT rate from exchange_rate"
+    );
     console.log(rows);
     res.status(200).json({});
   } catch (err) {
@@ -15,4 +93,9 @@ async function handleLogin(req, res) {
 
 module.exports = {
   handleLogin,
+  handleGetExchangeRate,
+  handleDeleteWallet,
+  handleGetCurrencyWallet,
+  handleGetWallet,
+  handlePostTransaction,
 };
