f6093aa2d45b4b66ec0abcbc7859695e266312eb
Tim <limz0166@e.ntu.edu.sg>
2022-07-22 16:22:32 +0800

    modified functionality 4 of appHandler.js

--- back/TT SQL queries.sql
+++ back/TT SQL queries.sql
@@ -5,7 +5,7 @@ SELECT * FROM `user`;
 SELECT * FROM `currency`;
 
 /*exchange rate data*/
-SELECT rate from `exchange_rate` WHERE exchange_currency = 'CAD';
+SELECT rate from `exchange_rate`;
 
 /*transaction data*/
 SELECT * from `transaction` WHERE  wallet_id = 1;
@@ -19,7 +19,11 @@ LEFT JOIN currency c
 ON w.user_id = c.wallet_id
 WHERE username = 'user101';
 
+INSERT INTO `transaction` (wallet_id, debit_id, debit_currency, debit_amount, 
+credit_id, credit_currency, credit_amount, `description`, created_by, updated_by)
+VALUES(1, 13, "JPY", 10, 1, "USD", 10, "Test", "Jacky", "Jacky");
 
+<<<<<<< HEAD
 /*delete data*/
 DELETE multicurrency.currency
 FROM multicurrency.currency
@@ -41,8 +45,11 @@ SET amount = amount + {value}
 WHERE wallet_id = {wallet_id} and currency = {credit_currency}
 >>>>>>> 7320c90 (Modified sql file)
 
-
-
+SELECT amount FROM user u 
+LEFT JOIN wallet w ON u.id =  w.user_id 
+LEFT JOIN currency c ON w.user_id = c.wallet_id 
+WHERE u.username = "user101" AND c.currency = "EUR" 
+AND w.id = 1;
 
 
 
